1b2dc4b9f2d484e42a35c68a24ab1d473e510351
Safi <48173372+SafiBadi@users.noreply.github.com>
2020-12-08 12:43:34 +0530

    Delete ex_08_05.py

--- ex_08_05 email count/ex_08_05.py
+++
@@ -1,9 +0,0 @@
-fname = input('Enter fname : ')
-fhand = open(fname)
-count = 0
-for line in fhand:
-    if line.startswith("From "):
-        email = line.split()
-        print(email[1])
-        count = count + 1
-print("There were",count,"lines in the file with From as the first word")
